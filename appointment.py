a900bbeeb4e87b1e7dabb9f5b0f3f9ceef6def90
Menaka Anandan <menakaanandan324@gmail.com>
2026-07-18 19:24:23 +0530

    ressolved merge conflict

---
+++ appointment.py
@@ -0,0 +1,12 @@
+patients=[]
+doctors={
+    "dr.smith(cardiologist)","dr.johnson(dermatologist)","dr.williams(orthopedic)"
+    ]
+def register_patient():
+    name=input("enter patient name:")
+    age =input"enter age:")
+    patients.append(patient)
+    print("patient registered successfully!\n")
+    
+    print("hospital management system")))"
+}
